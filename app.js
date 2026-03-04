fde3531fe7ac48e94d576647f8f73a3feb84ed6a
TumisoObed <cse25-201@thuto.bac.ac.bw>
2026-03-04 12:54:35 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.body.style.opacity = 0;
-  setTimeout(() => {
-    document.body.style.transition = "opacity 1s ease";
-    document.body.style.opacity = 1;
-  }, 100);
-});
